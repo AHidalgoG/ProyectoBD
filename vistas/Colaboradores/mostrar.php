7713555e33e8fa9e671a33797967a5ddca38fc89
alhidalgog <alonso.hidalgo004@gmail.com>
2025-06-08 00:44:07 -0400

    asdasdada s 
    :pw
    exit
    :pw
    
    asd
    
    
    
    
    
    n
    
    
    
    y
    i
    u
    y
    
    
    
    
    
    z
    
    z
    z
    
    
    
    
    
    
    nnn

---
+++ vistas/Colaboradores/mostrar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mostrar Colaboradores</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <th>RUT</th>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+        </thead>
+        <tbody>
+            <td>21532279-3</td>
+            <td>Alonso</td>
+            <td>Hidalgo Gutierrez</td>         
+        </tbody>
+    </table>
+</body>
+</html>
---
+++ vistas/Sucursales/mostrar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mostrar Sucursales</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <th>ID Sucursal</th>
+            <th>Nombre</th>
+            <th>Administrador</th>
+        </thead>
+        <tbody>
+            <td>001</td>
+            <td>Unimarc centro</td>
+            <td>Gabriel Casanueva</td>
+        </tbody>
+    </table>
+</body>
+</html>
